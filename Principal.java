b9a6ac896f59048d1a0c8944fba98208c2937fd9
alumno <alumno@ubuntu.myguest.virtualbox.org>
2025-02-05 09:56:44 +0100

    Añadi metodo fecha y hora

--- Principal.java
+++ Principal.java
@@ -1,5 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Principal{
 public static void main(String[] args){
 System.out.println("Hola Mundo");
+mostrarFechaHora();
+}
+public static void mostrarfechahora() {
+SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+System.out.println("Fecha y hora actual: " + sdf.format(new Date()));
 }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # trabajodam
 Repositorio de pruebas de Entornos de Desarollo curso 2024-2025
+
+## Fecha hora
+Añadi el metodo para mostrar la fecha y hora actual.
